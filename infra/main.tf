c1b9dce1c9caa88f731261579480fbafc3e0bfb2
ravic8 <ravi@chain8.org>
2025-06-14 02:51:57 +0530

    test 4

--- infra/main.tf
+++ infra/main.tf
@@ -80,8 +80,3 @@ resource "aws_instance" "clickhouse" {
   }
 }
 
-# 5) Expose the public IP
-output "clickhouse_ip" {
-  description = "Public IP of the ClickHouse EC2 instance"
-  value       = aws_instance.clickhouse.public_ip
-}
