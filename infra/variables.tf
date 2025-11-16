8e3e9ba9d222a16a5aa99d448d881b648c6b1f01
ravic8 <ravi@chain8.org>
2025-11-16 17:04:47 +0530

    v0.0 Not deploying resources in us-east-1

--- infra/variables.tf
+++ infra/variables.tf
@@ -8,6 +8,12 @@ variable "office_cidr" {
   description = "Your IP/CIDR for SSH & ClickHouse access"
 }
 
+variable "github_cidr" {
+  type        = string
+  description = "CIDR range allowed for GitHub Actions SSH"
+  default     = "0.0.0.0/0"  # for now, allow from anywhere; can tighten later
+}
+
 variable "key_name" {
   type        = string
   description = "Name of your existing AWS keypair"
